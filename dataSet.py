09325282ad0a8a144b9d26675cbb96f52e2c8345
MortarDefender <63960808+MortarDefender@users.noreply.github.com>
2022-01-01 21:17:43 +0200

    data set comment for albu,entation

--- dataSet.py
+++ dataSet.py
@@ -1,5 +1,10 @@
 
 
-class GetDataSet():
-    def __init__(self):
+# https://github.com/albumentations-team/albumentations
+class DataSetSystem():
+    def __init__(self, topic):
+        self.__topic = topic
+    
+    def getData():
+        # https://public.roboflow.com/object-detection
         pass
